e94d3d1a4ca0c3d53ff45cece956e31d0b7c736e
Anwesh43 <anweshthecool0@gmail.com>
2020-02-02 13:42:22 +0530

    adding SBPLNode class

--- stepbouncypolylineview/src/main/java/com/anwesh/uiprojects/stepbouncypolylineview/StepBouncyPolyLineView.kt
+++ stepbouncypolylineview/src/main/java/com/anwesh/uiprojects/stepbouncypolylineview/StepBouncyPolyLineView.kt
@@ -121,4 +121,45 @@ class StepBouncyPolyLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBPLNode(var i : Int, val state : State = State()) {
+
+        private var next : SBPLNode? = null
+        private var prev : SBPLNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SBPLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBPLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBPLNode {
+            var curr : SBPLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
