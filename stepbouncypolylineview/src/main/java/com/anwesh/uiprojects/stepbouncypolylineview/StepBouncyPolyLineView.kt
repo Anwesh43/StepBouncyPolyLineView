1df8c6a26952d661564c9ff7f1bab44535afe232
Anwesh43 <anweshthecool0@gmail.com>
2020-02-02 13:20:12 +0530

    adding StepBouncyPolyLineView class

--- stepbouncypolylineview/src/main/java/com/anwesh/uiprojects/stepbouncypolylineview/StepBouncyPolyLineView.kt
+++ stepbouncypolylineview/src/main/java/com/anwesh/uiprojects/stepbouncypolylineview/StepBouncyPolyLineView.kt
@@ -56,3 +56,21 @@ fun Canvas.drawSBPLNode(i : Int, scale : Float, paint : Paint) {
     drawStepBouncyPolyLines(scale, size, paint)
     restore()
 }
+
+class StepBouncyPolyLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
